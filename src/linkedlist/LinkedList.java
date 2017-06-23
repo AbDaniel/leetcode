9b725622aa6501e5a90a646437287ed313214b51
abdaniel <abrahamdaniel92@gmail.com>
2017-06-23 07:45:28 -0700

    Added Linkedlist node:

---
+++ src/linkedlist/LinkedList.java
@@ -0,0 +1,10 @@
+package linkedlist;
+
+import java.util.List;
+
+/**
+ * Created by daniel on 6/23/17.
+ */
+public class LinkedList {
+
+}
---
+++ src/linkedlist/ListNode.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+/**
+ * Created by daniel on 6/23/17.
+ */
+public class ListNode<T> {
+    T data;
+
+    ListNode<T> next;
+
+    public ListNode(T data) {
+        this.data = data;
+    }
+}
