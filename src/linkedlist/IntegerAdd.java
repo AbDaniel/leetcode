c32777cffb2aad67fa0c5cf5f4d837d7756f38e2
Williams <aw186034@musaw186034-903.attlocal.net>
2017-06-24 08:38:58 -0700

    [2] Problem no 2 List integer addition is solved

--- src/linkedlist/IntegerAdd.java
+++ src/linkedlist/IntegerAdd.java
@@ -8,6 +8,31 @@ import datastructure.LinkedList;
 public class IntegerAdd {
 
     public LinkedList<Integer> add(LinkedList<Integer> list1, LinkedList<Integer> list2) {
-        return null;
+        int carry = 0;
+
+        LinkedList<Integer> result = new LinkedList<>();
+        while (list1.length() > 0 && list2.length() > 0) {
+            int sum = list1.remove(0) + list2.remove(0);
+            sum += carry;
+            carry = sum / 10;
+            sum = sum % 10;
+
+            result.add(sum);
+        }
+
+        LinkedList<Integer> list = list1.length() != 0 ? list1 : list2;
+
+        while (list.length() > 0) {
+            int sum = list.remove(0);
+            sum += carry;
+            carry = sum / 10;
+            sum = sum % 10;
+
+            result.add(sum);
+        }
+
+        if (carry != 0) result.add(carry);
+
+        return result;
     }
 }
--- test/linkedlist/IntegerAddTest.java
+++ test/linkedlist/IntegerAddTest.java
@@ -13,6 +13,32 @@ public class IntegerAddTest {
     public void add() throws Exception {
         LinkedList<Integer> list1 = new LinkedList<>(2, 4, 3);
         LinkedList<Integer> list2 = new LinkedList<>(5, 6, 4);
+
+        IntegerAdd add = new IntegerAdd();
+
+        LinkedList<Integer> actual = add.add(list1, list2);
+
+        LinkedList<Integer> expected = new LinkedList<>(7, 0, 8);
+        assertEquals(expected, actual);
+
+        list1 = new LinkedList<>(2, 4, 3);
+        list2 = new LinkedList<>(9);
+
+
+        actual = add.add(list1, list2);
+
+        expected = new LinkedList<>(1, 5, 3);
+        assertEquals(expected, actual);
+
+        list1 = new LinkedList<>(3);
+        list2 = new LinkedList<>(5, 6, 4);
+
+
+        actual = add.add(list1, list2);
+
+        expected = new LinkedList<>(8, 6, 4);
+        assertEquals(expected, actual);
+
     }
 
 }
